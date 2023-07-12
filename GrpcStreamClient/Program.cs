87362d5dc77568e5132655fab0dfaf911e2dc924
hclcc <hclcruz@gmail.com>
2023-07-12 13:30:28 +0100

    Add project files.

---
+++ GrpcStreamClient/Program.cs
@@ -0,0 +1,93 @@
+﻿
+using Google.Protobuf.WellKnownTypes;
+
+using Grpc.Net.Client;
+
+using GrpcServer;
+
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace GrpcStreamClient
+{
+    public class Program
+    {
+        private static Random random;
+        public static async Task Main(string[] args)
+        {
+            //await ServerStreamingDemo();
+            await BidirectionalStreamingDemo();
+            Console.ReadKey();
+        }
+
+        private static async Task BidirectionalStreamingDemo()
+        {
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new StreamDemo.StreamDemoClient(channel);
+            var stream = client.BidirectionalStreamingDemo();
+
+            var requestTask = Task.Run(async () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    random = new Random();
+                    var randomnunber = random.Next(1, 10);
+                    await Task.Delay(randomnunber * 1000);
+                    await stream.RequestStream.WriteAsync(new Test { Testmessage = i.ToString() });
+                    Console.WriteLine("Sent request: " + i.ToString());
+                }
+                await stream.RequestStream.CompleteAsync();
+
+            });
+            var responseTask = Task.Run(async () =>
+            {
+                while (await stream.ResponseStream.MoveNext(CancellationToken.None))
+                {
+                    Console.WriteLine("Received response: " + stream.ResponseStream.Current.Testmessage);
+                }
+                Console.WriteLine("Response stream completed: " );
+
+            });
+
+            await Task.WhenAll(requestTask, responseTask);
+        }
+
+        private static async Task ClientStreamingDemo()
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new StreamDemo.StreamDemoClient(channel);
+
+            var stream = client.ClientStreamingDemo();
+            for (int i = 0; i < 10; i++)
+            {
+                await stream.RequestStream.WriteAsync(new Test { Testmessage = $"Message {i}" });
+            }
+            stream.RequestStream.CompleteAsync();
+            Console.WriteLine("Complete Client Streaming");
+        }
+
+
+        private static async Task ServerStreamingDemo()
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new StreamDemo.StreamDemoClient(channel);
+            var response = client.SterverStreamingDemo(new Test { Testmessage = "Test" });
+
+            while (await response.ResponseStream.MoveNext(CancellationToken.None))
+            {
+                var value = response.ResponseStream.Current.Testmessage;
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine("complete");
+
+
+            await channel.ShutdownAsync();
+        }
+
+    }
+
+}
---
+++ gRPCServerStream/Services/StreamDemoService.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Core;
+
+using GrpcServer;
+
+
+namespace GrpcServer.Services
+{
+    public class StreamDemoService : StreamDemo.StreamDemoBase
+    {
+        private Random random;
+
+        public StreamDemoService()
+        {
+            random = new Random();
+        }
+
+        public override async Task SterverStreamingDemo(Test request, IServerStreamWriter<Test> responseStream, ServerCallContext context)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                await responseStream.WriteAsync(new Test { Testmessage = $"Menssagem {i.ToString()}" });
+                var randomnumber = random.Next(1, 10);
+                await Task.Delay(randomnumber * 1000);
+            }
+        }
+        public override async Task<Test> ClientStreamingDemo(IAsyncStreamReader<Test> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                Console.WriteLine(requestStream.Current.Testmessage);
+            }
+            Console.WriteLine("Client Streaming Complete");
+            return new Test { Testmessage = "Sample" };
+        }
+        public override async Task BidirectionalStreamingDemo(IAsyncStreamReader<Test> requestStream, IServerStreamWriter<Test> responseStream, ServerCallContext context)
+        {
+            var tasks = new List<Task>();
+            while (await requestStream.MoveNext())
+            {
+                Console.WriteLine("Received request: " + requestStream.Current.Testmessage);
+                var task = Task.Run(async () =>
+                {
+                    var message = requestStream.Current.Testmessage;
+                    var randomnunber = random.Next(1, 10);
+                    await Task.Delay(randomnunber * 1000);
+                    await responseStream.WriteAsync(new Test { Testmessage = message });
+                    Console.WriteLine($"Sent Respomse : {message}");
+                }
+                );
+                tasks.Add(task);
+            }
+            await Task.WhenAll(tasks);
+            Console.WriteLine("Bidirectional Streaming Complete");
+
+        }
+    }
+}
